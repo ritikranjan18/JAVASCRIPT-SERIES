666671fcba2735f67e9872445c672543e3e5f591
Ritik Ranjan <143780118+ritikranjan18@users.noreply.github.com>
2026-04-10 11:40:04 +0000

    conversion of datatype

---
+++ 01_Basics/conversionOperation.js
@@ -0,0 +1,43 @@
+// let score1="33";
+// let score2="33abc";
+// let score3=null;
+// let score4=undefined;
+// let score5=true;
+// console.log(typeof score);
+// console.log(typeof(score1)); //method
+
+// let valueInNumber1=Number(score1);
+// let valueInNumber2=Number(score2);
+// let valueInNumber3=Number(score3);
+// let valueInNumber4=Number(score4);
+// let valueInNumber5=Number(score5);
+// console.log(typeof(valueInNumber1));
+// console.log(valueInNumber1);
+// console.log(typeof(valueInNumber2));
+// console.log(valueInNumber2);
+// console.log(typeof(valueInNumber3));
+// console.log(valueInNumber3);
+// console.log(typeof(valueInNumber4));
+// console.log(valueInNumber4);
+// console.log(typeof(valueInNumber5));
+// console.log(valueInNumber5);
+
+
+
+// "33"=>33
+// "33abc"=>NaN  =>type:number
+// true=>1;false=>0;
+
+let isLoggedIn="";
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+// 1=>true 0=>fasle;
+//"" => false;
+// "ritik"=true;
+
+let someNumber=33;
+let stringnumber=String(someNumber);
+console.log(stringnumber);
+console.log(typeof stringnumber)
