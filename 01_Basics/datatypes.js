a8c46c6e72f145d9b99135ecef87ba722a5d70b0
Ritik Ranjan <143780118+ritikranjan18@users.noreply.github.com>
2026-04-10 11:27:18 +0000

    datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treate all JS code as newer version
+//alert(3+3); //we are using node js not browser
+
+console.log(3+
+    3); //code readablity is important 
+console.log("riitk");
+
+let name="Ritik"; //String 
+let age=18; // number
+let isLog=false //boolean
+
+
+//number=>2 to power 53
+//bigint
+//string=>"";
+//boolean=> true/false;
+//null=>standalone value
+// undefined => value is not assign yet
+//symbol=>to find uniqueness 
+
+
+//object
+console.log(typeof "RItik")
+console.log(typeof age);
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
