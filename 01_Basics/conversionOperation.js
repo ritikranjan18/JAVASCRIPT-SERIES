b5ed871456a72f6a58cf6a64a8cd0f82947c1d68
Ritik Ranjan <143780118+ritikranjan18@users.noreply.github.com>
2026-04-10 11:52:25 +0000

    conversion

--- 01_Basics/conversionOperation.js
+++ 01_Basics/conversionOperation.js
@@ -28,16 +28,50 @@
 // "33abc"=>NaN  =>type:number
 // true=>1;false=>0;
 
-let isLoggedIn="";
-let booleanIsLoggedIn=Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// let isLoggedIn="";
+// let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 
 // 1=>true 0=>fasle;
 //"" => false;
 // "ritik"=true;
 
-let someNumber=33;
-let stringnumber=String(someNumber);
-console.log(stringnumber);
-console.log(typeof stringnumber)
+// let someNumber=33;
+// let stringnumber=String(someNumber);
+// console.log(stringnumber);
+// console.log(typeof stringnumber)
+
+
+//******************************Operation**************
+let value=3;
+let negvalue=-value;
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1="Hello";
+// let str2="Ritik";
+
+// let str3=str1+str2;
+// console.log(str3);
+// console.log("1"+2+3); //if string first then all converted to string taht why 123
+// console.log(1+"2");
+// console.log(1+2+"2"); //if number first then all are converted to nymber that why 32 not 122
+
+// console.log(+true);  
+// console.log(+"");
+
+let num1,num2,num3;
+num1=num2=num3=2+2;
+
+let gamecounter=100;
+gamecounter++; //prefix;
+++gamecounter; //psotfix;
+
+console.log(gamecounter);
