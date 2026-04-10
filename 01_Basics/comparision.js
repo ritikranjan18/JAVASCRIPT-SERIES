7d587e2ebe11f6a4bb58bfb404179a9bc9935a3d
Ritik Ranjan <143780118+ritikranjan18@users.noreply.github.com>
2026-04-10 12:02:00 +0000

    comparsion

---
+++ 01_Basics/comparision.js
@@ -0,0 +1,23 @@
+
+//we should comapre same datatype
+
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// console.log(undefined==0)
+
+// ===  > The === (strict equality) operator in JavaScript compares both value and data type without performing type coercion. 
+//It returns true only if both operands are identical in type and value, making it more predictable than ==, which coerces types.
+//Using === is generally recommended for safer comparisons. 
+
+console.log("2"===2);
