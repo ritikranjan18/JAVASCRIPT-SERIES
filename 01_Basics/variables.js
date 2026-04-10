7e8a04346a5064c793913017f00e573a132a02ba
Ritik Ranjan <143780118+ritikranjan18@users.noreply.github.com>
2026-04-10 11:00:15 +0000

    commit variable

---
+++ 01_Basics/variables.js
@@ -0,0 +1,20 @@
+const accountId=1333;
+let accountEmail="rititik@gmail.com";
+var accoundPassword="12345";
+accountCity="Jaipur";
+
+let accountState;
+//accountId=2; //not allowed
+accountEmail="hello@gmail.com";
+accoundPassword="1245";
+accountCity="Muzaffarpur";
+
+console.log(accountId);
+
+console.table([accoundPassword,accountEmail,accountId,accountCity,accountState]);
+
+/*
+Prefer not to use var becuase of issue in block scope and fucntional scope
+ */
+
+
