d741c8c6b30fc57692ee58a6d0c782afc3f4e16d
Ritik Ranjan <143780118+ritikranjan18@users.noreply.github.com>
2026-04-11 10:57:08 +0000

    JSON

--- 02_Basics/Object2.js
+++ 02_Basics/Object2.js
@@ -1,35 +1,53 @@
-//singletion
+// //singletion
 
-const tinderUser=new Object()
-tinderUser.id="123abc"
-tinderUser.name="Samay"
-// console.log(tinderUser)
+// const tinderUser=new Object()
+// tinderUser.id="123abc"
+// tinderUser.name="Samay"
+// // console.log(tinderUser)
 
-const regualarUser={
-    email:"some@gmail.com",
-    fullname:{
-        UserFullname:{
-            firstname:"Ritik",
-            lastname:"Ranjan"
-        }
-    }
+// const regualarUser={
+//     email:"some@gmail.com",
+//     fullname:{
+//         UserFullname:{
+//             firstname:"Ritik",
+//             lastname:"Ranjan"
+//         }
+//     }
+// }
+// // console.log(regualarUser);
+
+// const obje1={1:"a",2:"b"}
+// const obje2={3:"c",4:"d"}
+// const obj=Object.assign(obje1,obje2)
+// const objj=Object.assign({},obje1,obje2)
+
+// const objjj={...obje1,...obje2}  //spread
+// console.log(obj)  
+// console.log(objj);
+
+// console.log(objjj);
+
+// console.log(Object.keys(tinderUser))
+
+// console.log(Object.values(tinderUser))
+
+// console.log(Object.entries(tinderUser))
+
+//object destructuring
+const course={
+    courseName:"Javascript",
+    price:999,
+    courseInstructor:"Ritik"
 }
-// console.log(regualarUser);
 
-const obje1={1:"a",2:"b"}
-const obje2={3:"c",4:"d"}
-const obj=Object.assign(obje1,obje2)
-const objj=Object.assign({},obje1,obje2)
+const {courseInstructor:Instructor}=course
+console.log(Instructor);
 
-const objjj={...obje1,...obje2}  //spread
-console.log(obj)  
-console.log(objj);
+//++++  API  ++++++
 
-console.log(objjj);
-
-console.log(Object.keys(tinderUser))
-
-console.log(Object.values(tinderUser))
-
-console.log(Object.entries(tinderUser))
+// { //JSON
+//     name:"Ritik",
+//     coursename:"JavaScript",
+//     price:"free"
+// }
 
