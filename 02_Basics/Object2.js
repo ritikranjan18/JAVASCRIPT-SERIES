6192dd855d2158532ecf3b68a42b461d9148bd76
Ritik Ranjan <143780118+ritikranjan18@users.noreply.github.com>
2026-04-11 10:01:34 +0000

    object2

---
+++ 02_Basics/Object2.js
@@ -0,0 +1,35 @@
+//singletion
+
+const tinderUser=new Object()
+tinderUser.id="123abc"
+tinderUser.name="Samay"
+// console.log(tinderUser)
+
+const regualarUser={
+    email:"some@gmail.com",
+    fullname:{
+        UserFullname:{
+            firstname:"Ritik",
+            lastname:"Ranjan"
+        }
+    }
+}
+// console.log(regualarUser);
+
+const obje1={1:"a",2:"b"}
+const obje2={3:"c",4:"d"}
+const obj=Object.assign(obje1,obje2)
+const objj=Object.assign({},obje1,obje2)
+
+const objjj={...obje1,...obje2}  //spread
+console.log(obj)  
+console.log(objj);
+
+console.log(objjj);
+
+console.log(Object.keys(tinderUser))
+
+console.log(Object.values(tinderUser))
+
+console.log(Object.entries(tinderUser))
+
