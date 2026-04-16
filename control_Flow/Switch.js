4198510bdb9a3aa6f132c8e1bd01a4f3483a72d6
Ritik Ranjan <143780118+ritikranjan18@users.noreply.github.com>
2026-04-16 07:05:23 +0000

    switch

---
+++ control_Flow/Switch.js
@@ -0,0 +1,27 @@
+// switch(key){
+//     case value:
+//         break;
+//     default:
+//         break;
+// }
+
+const month=3
+switch(month){
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("Feb")
+        break;
+    case 2:
+        console.log("March")
+        break;
+    case 2:
+        console.log("April")
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+        
+}
